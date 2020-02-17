dd35f9702681adcd39504eaff72da38a9363bbb9
Jessie Tarrosa <creators.nuke@gmail.com>
2020-02-17 03:21:23 +0000

    Updates

---
+++ precache-manifest.56b99b8b1b0c9ac2d234e742cc2935eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "710ece8812364ec587c5879982390e96",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "b0afc706de518678b041",
+    "url": "/react-calculator/static/css/main.233b5c5f.chunk.css"
+  },
+  {
+    "revision": "8ec093b113652853c041",
+    "url": "/react-calculator/static/js/2.82be7326.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-calculator/static/js/2.82be7326.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0afc706de518678b041",
+    "url": "/react-calculator/static/js/main.33b4a7bf.chunk.js"
+  },
+  {
+    "revision": "36013ae44c0d5168c12c",
+    "url": "/react-calculator/static/js/runtime-main.c0112216.js"
+  }
+]);
